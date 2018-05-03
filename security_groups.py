94cb8b659c9e60ec43e88a491c6b3c347a1e8ca6
Paul Macdonnell <pmacdonnell@creditsense.com.au>
2018-05-03 13:21:13 +1000

    Push the little daisies

--- security_groups.py
+++ security_groups.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """List the security groups"""
 import boto3
+import sys
 from botocore.exceptions import ClientError
 
-def main():
+def main(filter_text="*"):
     """Do the main things"""
     ec2 = boto3.client('ec2')
 
     try:
-        response = ec2.describe_security_groups()
+        response = ec2.describe_security_groups(Filters=[{'Name': 'tag:Name','Values': [filter_text]}])
+        if len(response['SecurityGroups']) == 0 and filter_text[:3] == 'sg-':
+        	response = ec2.describe_security_groups(GroupIds=[filter_text])
+	
         #pprint(response['SecurityGroups'])
         for group in sorted(response['SecurityGroups'], key=lambda k: k['Description']):
             print("{}: {}".format(group['GroupId'], group['Description']))
@@ -18,9 +22,10 @@ def main():
                     print("\tAllow {} {}-{} from {}".format(rule['IpProtocol'], rule.get('FromPort', 'all'), rule.get('ToPort', 'all'), iprange['CidrIp']))
                 for sg in sorted(rule.get('UserIdGroupPairs', []), key=lambda m: m['GroupId']):
                     print("\tAllow {}".format(sg['GroupId']))
+                    #main(sg['GroupId'])
 
     except ClientError as c_e:
         print(c_e)
 
 if __name__ == '__main__':
-    main()
+    _= [main(text) for text in sys.argv[1:]]
