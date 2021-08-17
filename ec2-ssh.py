abfa9cb559f9055122a8d2c4a966f655dfc7adb1
Paul Macdonnell <paul.macdonnell@neto.com.au>
2021-08-17 17:37:54 +1000

    Allow filtering by PrivateIP address or Instance ID. ssh directly from the command, rather than forking a new terminal

--- ec2-ssh.py
+++ ec2-ssh.py
@@ -2,9 +2,11 @@
 '''EC2 ssh with filter'''
 
 
-import sys
-import argparse
-import os
+from sys import argv
+from platform import system
+from os import environ
+from os import system as sysexec
+from argparse import ArgumentParser
 from subprocess import Popen
 
 
@@ -30,9 +32,14 @@ def display_instance(inst):
 def describe_ec2(filter_args):
     '''List AWS EC2 instances'''
     import boto3
+
+    _filter_name = 'tag:Name'
+    if filter_args.privateip: _filter_name = 'network-interface.addresses.private-ip-address'
+    if filter_args.instanceid: _filter_name = 'instance-id'
+
     ec2 = boto3.client('ec2')
     for text in filter_args.text:
-        response = ec2.describe_instances(Filters=[{'Name': 'network-interface.addresses.private-ip-address' if filter_args.ip else 'tag:Name','Values': [text]},{'Name': 'instance-state-name', 'Values': ['running']}])
+        response = ec2.describe_instances(Filters=[{'Name': _filter_name, 'Values': [text]}, {'Name': 'instance-state-name', 'Values': ['running']}])
         results = [display_instance(item) for res in response['Reservations']
                                           for item in res['Instances']]
         ssh_ec2(results, filter_args)
@@ -40,17 +47,29 @@ def describe_ec2(filter_args):
 
 def ssh_ec2(instances, fargs):
     '''Display SSH command for list of instances'''
+
+    terminal_clients = {
+        'Darwin': 'iterm',
+        'Linux':  ['x-terminal-emulater', '-e'],
+    }
+
     _ = [print("ssh {:15}\t{:18}\t{}".format(server['PrivateIpAddress'], server['InstanceId'], server['Name'])) for server in sorted(instances, key=lambda k: k['Name'])]
     if fargs.connect:
-        _ = [Popen(["x-terminal-emulator", "-e", "ssh", "{}@{}".format(fargs.user, server['PrivateIpAddress'])]) for server in sorted(instances, key=lambda k: k['Name'])]
+        if fargs.user:
+            #_ = [Popen([terminal_clients[system()], "ssh", "{}@{}".format(fargs.user, server['PrivateIpAddress'])]) for server in sorted(instances, key=lambda k: k['Name'])]
+            _ = [sysexec("ssh {}@{}".format(fargs.user, server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
+        else:
+            #_ = [Popen([terminal_clients[system()], "ssh", "{}".format(server['PrivateIpAddress'])]) for server in sorted(instances, key=lambda k: k['Name'])]
+            _ = [sysexec("ssh {}".format(server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("text", help="String to search for in the NAME tag of EC2 instances", nargs="+")
-    parser.add_argument("-i", "--ip", help="Search for the IP address of matching EC2 instance(s)", action="store_true")
+    parser.add_argument("-i", "--instanceid", help="Search for EC2 instance(s) using the Instance ID", action="store_true")
+    parser.add_argument("-p", "--privateip", help="Search for EC2 instance(s) using the IP address", action="store_true")
     parser.add_argument("-c", "--connect", help="Connect via SSH to the EC2 instance(s)", action="store_true")
-    parser.add_argument("-u", "--user", help="User to connect as", default=os.environ.get('USER'))
+    parser.add_argument("-u", "--user", help="User to connect as")
     args = parser.parse_args()
-    #results = [describe_ec2(text) for text in sys.argv[1:]]
+    #results = [describe_ec2(text) for text in argv[1:]]
     describe_ec2(args)
