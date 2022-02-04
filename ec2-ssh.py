be561bbba4ab48b0dd3c60d9134404017dfc7957
Paul Macdonnell <paulmacdonnell@geniesolutions.com.au>
2022-02-04 11:01:16 +1000

    Add in SSM as a connection method. Add in an earlier step to display all of the instances we'll be connecting to, separating the information display from the actual connection

--- ec2-ssh.py
+++ ec2-ssh.py
@@ -13,12 +13,13 @@ from subprocess import Popen
 def get_tag(arr, tagname='Name'):
     '''Just get a single tag'''
     for tag in arr:
-        if tag['Key'] == tagname: return tag['Value']
+        if tag['Key'] == tagname:
+            return tag['Value']
 
 
 def display_instance(inst):
     '''Display an instances details'''
-    #print(inst)
+    # print(inst)
     dispinst = {}
     dispinst['PrivateIpAddress'] = inst['PrivateIpAddress']
     dispinst['KeyName'] = inst.get('KeyName', 'ansible_ec2_key')
@@ -34,33 +35,49 @@ def describe_ec2(filter_args):
     import boto3
 
     _filter_name = 'tag:Name'
-    if filter_args.privateip: _filter_name = 'network-interface.addresses.private-ip-address'
-    if filter_args.instanceid: _filter_name = 'instance-id'
+    if filter_args.privateip:
+        _filter_name = 'network-interface.addresses.private-ip-address'
+    if filter_args.instanceid:
+        _filter_name = 'instance-id'
 
     ec2 = boto3.client('ec2')
     for text in filter_args.text:
         response = ec2.describe_instances(Filters=[{'Name': _filter_name, 'Values': ["*{}*".format(text)]}, {'Name': 'instance-state-name', 'Values': ['running']}])
         results = [display_instance(item) for res in response['Reservations']
-                                          for item in res['Instances']]
-        ssh_ec2(results, filter_args)
+                   for item in res['Instances']]
+        list_ec2(results, filter_args)
+        if filter_args.connect:
+            ssh_ec2(results, filter_args)
+        if filter_args.ssm:
+            ssm_ec2(results, filter_args)
+
+
+def list_ec2(instances, fargs):
+    '''Display the list of instances to connect to'''
+    print("Found {} instances to connect to".format(len(instances)))
+    _ = [print("Connecting to {:18}\t{}".format(server['InstanceId'], server['Name'])) for server in sorted(instances, key=lambda k: k['Name'])]
 
 
 def ssh_ec2(instances, fargs):
-    '''Display SSH command for list of instances'''
+    '''Connect to instances via SSH command'''
 
     terminal_clients = {
         'Darwin': 'iterm',
         'Linux':  ['x-terminal-emulater', '-e'],
     }
 
-    _ = [print("ssh {:15}\t{:18}\t{}".format(server['PrivateIpAddress'], server['InstanceId'], server['Name'])) for server in sorted(instances, key=lambda k: k['Name'])]
-    if fargs.connect:
-        if fargs.user:
-            #_ = [Popen([terminal_clients[system()], "ssh", "{}@{}".format(fargs.user, server['PrivateIpAddress'])]) for server in sorted(instances, key=lambda k: k['Name'])]
-            _ = [sysexec("ssh {}@{}".format(fargs.user, server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
-        else:
-            #_ = [Popen([terminal_clients[system()], "ssh", "{}".format(server['PrivateIpAddress'])]) for server in sorted(instances, key=lambda k: k['Name'])]
-            _ = [sysexec("ssh {}".format(server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
+    #_ = [print("ssh {:15}\t{:18}\t{}".format(server['PrivateIpAddress'], server['InstanceId'], server['Name'])) for server in sorted(instances, key=lambda k: k['Name'])]
+    if fargs.user:
+        _ = [sysexec("ssh {}@{}".format(fargs.user, server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
+    else:
+        _ = [sysexec("ssh {}".format(server['PrivateIpAddress'])) for server in sorted(instances, key=lambda k: k['Name'])]
+
+
+def ssm_ec2(instances, fargs):
+    '''Connect to the EC2 instance(s) via an SSM Session'''
+
+    #_ = [print("Connecting to {:18}\t{}".format(server['InstanceId'], server['Name'])) for server in sorted(instances, key=lambda k: k['Name'])]
+    _ = [sysexec("aws ssm start-session --target {}".format(server['InstanceId'])) for server in sorted(instances, key=lambda k: k['Name'])]
 
 
 if __name__ == '__main__':
@@ -68,7 +85,8 @@ if __name__ == '__main__':
     parser.add_argument("text", help="String to search for in the NAME tag of EC2 instances", nargs="+")
     parser.add_argument("-i", "--instanceid", help="Search for EC2 instance(s) using the Instance ID", action="store_true")
     parser.add_argument("-p", "--privateip", help="Search for EC2 instance(s) using the IP address", action="store_true")
-    parser.add_argument("-c", "--connect", help="Connect via SSH to the EC2 instance(s)", action="store_true")
+    parser.add_argument("-c", "--connect", "--ssh", help="Connect via SSH to the EC2 instance(s)", action="store_true")
+    parser.add_argument("-s", "--ssm", help="Connect via a SSM Session to the EC2 instance(s)", action="store_true")
     parser.add_argument("-u", "--user", help="User to connect as")
     args = parser.parse_args()
     #results = [describe_ec2(text) for text in argv[1:]]
